2d50ed15cc84f425323449d7f3ef57f5a190cc90
octronnnnnn <108590339+octronnnnnn@users.noreply.github.com>
2023-10-17 16:59:06 +0200

    finished task

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
-
-};
+const repeatString = function(string, n) {
+    let result = "";
+    if (n < 0) {
+        return "ERROR";
+    }
+    for (let i = 0; i < n; i++) {
+        result += string;
+    }
+    return result;
+}
+ 
 
 // Do not edit below this line
 module.exports = repeatString;
