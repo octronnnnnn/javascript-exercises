d0447159153f5fc47754354656fae54ffd7dc64a
octronnnnnn <108590339+octronnnnnn@users.noreply.github.com>
2023-10-18 13:08:47 +0200

    passes all tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
-
+const sumAll = function(x, y) {
+    let total = 0;
+    if (x < 0 || y < 0 || typeof x !== "number" || typeof y !== "number") {
+        return "ERROR";
+    }
+    if (x < y) {
+        while (x != y+1) {
+            total += x;
+            x++;
+        }
+        return total;
+    }
+    else if (x > y) {
+        while (x != y-1) {
+            total += x;
+            x--;
+        }
+        return total;
+    }
+    else {
+        return "ERROR";
+    }
 };
 
 // Do not edit below this line
