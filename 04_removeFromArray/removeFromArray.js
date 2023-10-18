59ac2cdfca6d20338eef5881cf52e9dc752132b2
octronnnnnn <108590339+octronnnnnn@users.noreply.github.com>
2023-10-18 12:34:06 +0200

    passes all tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) {
+    const newArray = [];
+    array.forEach(item => {
+        if (!args.includes(item)) {
+            newArray.push(item);
+            console.log(newArray);
+        };
+    });  
+    return newArray; 
 };
 
 // Do not edit below this line
