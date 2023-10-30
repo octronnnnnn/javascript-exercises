d83e7b07c055d903920ec003f6ecb5eab59117b6
octronnnnnn <108590339+octronnnnnn@users.noreply.github.com>
2023-10-30 11:22:07 +0100

    done

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	/* const sum = arr.reduce((accumulator, currentValue) =>  {
+     return accumulator + currentValue
+  }, 0);
+  return sum; */
+  const initialValue = 0;
+  return arr.reduce((product, currentValue) => product + currentValue, initialValue);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((product, currentValue) => product * currentValue);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  let product = 1;
+  for (let i = n; i > 0; i--) {
+    product *= i;
+  }	
+  return product;
 };
 
 // Do not edit below this line
